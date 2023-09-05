9848440eb2c18b974210775d6ba42d0c809fc93b
Tim Twigg <tim@twiggusa.com>
2023-09-05 13:51:45 -0700

    Update lines.py

--- lines.py
+++ lines.py
@@ -1,11 +1,13 @@
-# updated 6 August 2022
+# updated 5 September 2023
 # Counts the number of lines in code files
 
 from pathlib import Path
 
 breakdown = ["\nCode Files:"]
+fileCount = 0
 
-def countlines(f_list) :
+def countlines(f_list):
+    global breakdown, fileCount
     count = 0
     for file in f_list :
         startcount = count
@@ -13,30 +15,33 @@ def countlines(f_list) :
             for _ in f :
                 count += 1
         breakdown.append(f"  {file.name:25} | {count - startcount}")
+        fileCount += 1
     return count
 
 p = Path()
 libP = p / "lib"
 files = [i.resolve() for i in libP.iterdir() if i.name != "json.hpp"]
 count1 = countlines(files)
-print("\nCode Files: {} lines".format(count1))
+print(f"\nCode Files: {count1} lines ({len(files)} files)")
 
 breakdown.append("\nTest Files:")
 testP = p / "tests"
 files = [i.resolve() for i in testP.iterdir()]
 count2 = countlines(files)
-print(f"Test Files: {count2}")
+print(f"Test Files: {count2} lines ({len(files)} files)")
 
 data_dirs = ["data", "infodata"]
 count3 = 0
+fcount = 0
 for dir in data_dirs :
     breakdown.append(f"\nData Files - {dir}:")
     p2 = p / dir
     files = [i.resolve() for i in p2.iterdir()]
+    fcount += len(files)
     count3 += countlines(files)
-print("Data Files: {} lines".format(count3))
+print(f"Data Files: {count3} lines ({fcount} files)")
 
-print("Total: {} lines".format(count1 + count2 + count3))
+print(f"Total: {count1 + count2 + count3} lines ({fileCount} files)")
 
 for l in breakdown:
     print(l)
